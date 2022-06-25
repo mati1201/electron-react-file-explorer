7e91f50f3a840318808de37674d4b906f61b552b
unknown <mati.kaczmarek@op.pl>
2022-06-25 20:38:05 +0200

    Electron setup, create window and serve React App

---
+++ electron/main.ts
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const remoteMain = require('@electron/remote/main');
+
+remoteMain.initialize();
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  remoteMain.enable(window.webContents)
+
+  window.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow)
